5e02abe612dc40543b3474bfee346d2e024bac6d
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-04 10:40:14 +0200

    Actualizado título de la aplicación, agregados componentes de la página de inicio de gifs y la lista de tarjetas, actualizado componente de la barra lateral, actualizado módulo compartido.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +11,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
     GifsModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent { }
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) { }
+
+  get tags(): string[] {
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string): void {
+    this.gifsService.searchTag(tag);
+  }
+
+}
