fcf276de8742f062c1472b038c5377d3a5f7d07d
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-04 10:45:42 +0200

    Añadir carga de historial desde el almacenamiento local en el constructor de GifsService

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,7 @@ export class GifsService {
   private apiKey: string = 'gmNJmltTf06X15rvv6pQ3pYrvwcnJv80';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { this.loadLocalStorage(); }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -24,6 +24,20 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
